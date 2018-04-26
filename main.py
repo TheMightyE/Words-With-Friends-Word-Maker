7878641ceacac816d329f563f3100e0008913237
TheMightyE <sk2293@njit.edu>
2018-04-26 11:01:12 -0400

    Added wildcard functionality

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from sets import Set
 import re
 import Word as w
 
+
 def main():
+    global debug
+    debug = False
     letters = sys.argv[1]
     words = getWords(letters)
     scoredWords = []
@@ -17,35 +20,36 @@ def main():
     for i in newlist:
         print (i.word + ': ' + str(i.points))
 
+
 def score(word):
     ''' Define what each letter is worth in the game '''
     letterPoints = {
-        'a':1,
-        'b':4,
-        'c':4,
-        'd':2,
-        'e':1,
-        'f':4,
-        'g':3,
-        'h':3,
-        'i':1,
-        'j':10,
-        'k':5,
-        'l':2,
-        'm':4,
-        'n':2,
-        'o':1,
-        'p':4,
-        'q':10,
-        'r':1,
-        's':1,
-        't':1,
-        'u':2,
-        'v':5,
-        'w':4,
-        'x':8,
-        'y':3,
-        'z':10
+        'a': 1,
+        'b': 4,
+        'c': 4,
+        'd': 2,
+        'e': 1,
+        'f': 4,
+        'g': 3,
+        'h': 3,
+        'i': 1,
+        'j': 10,
+        'k': 5,
+        'l': 2,
+        'm': 4,
+        'n': 2,
+        'o': 1,
+        'p': 4,
+        'q': 10,
+        'r': 1,
+        's': 1,
+        't': 1,
+        'u': 2,
+        'v': 5,
+        'w': 4,
+        'x': 8,
+        'y': 3,
+        'z': 10
     }
     ''' Calculate the score '''
     score = 0
@@ -53,11 +57,18 @@ def score(word):
         score = score + letterPoints[ch]
     return score
 
+
 def getWords(letters):
     ''' Using the list that is used by the game '''
     filename = 'enable-word-list.txt'
 
+    # TODO: Add wildcars functionality
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    n = getOccurrence(letters, '?')
+    wildcard = '?' in letters
+
     possibleWords = []
+
     with open(filename) as inF:
         for line in inF:
             ''' Remove characters like the new line character '''
@@ -73,6 +84,8 @@ def getWords(letters):
                 tmp = letters
                 for char in line:
                     if char in tmp:
+                        if (debug):
+                            print('Found: ' + char)
                         good = True
                         '''
                         Every time a letter from the tmp string is checked,
@@ -80,19 +93,33 @@ def getWords(letters):
                         letters string has duplicate letters.
                         '''
                         tmp = removeLetter(tmp, char)
+
+                    elif hasWildcard(tmp):
+                        '''
+                        If letter was not found, then check if there is a
+                        wildcard present. If so, simply pass the letter as good.
+                        '''
+                        if (debug):
+                            print('Used Wildcard: ' + char)
+                        good = True
+                        tmp = removeLetter(tmp, '?')
+
                     else:
                         good = False
                         break
+                        
                 if good:
                     possibleWords.append(line)
 
     return possibleWords
 
+
 def replaceNonAlnum(s):
     p = re.compile(r'[^a-zA-Z0-9:-]+')
-    #p = re.compile(r'\W+') # replace whitespace chars
-    new = p.sub(' ',s)
+    # p = re.compile(r'\W+') # replace whitespace chars
+    new = p.sub(' ', s)
     return new.strip()
+
 
 def removeLetter(s, c):
     indxToRemove = s.find(c)
@@ -102,8 +129,21 @@ def removeLetter(s, c):
             newStr = newStr + s[i]
     return newStr
 
+
+def getOccurrence(s, c):
+    n = 0
+    for char in s:
+        if char == c:
+            n = n + 1
+    return n
+
+
+def hasWildcard(s):
+    return '?' in s
+
+
 main()
 
 
-#local group = few dozen small
-#major galaxy in local group = 2 or 3
+# local group = few dozen small
+# major galaxy in local group = 2 or 3
